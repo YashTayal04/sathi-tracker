48ca6a03fe57a5b08599796205fe70e4ad6d75d7
Yash Tayal <yashtayal341@gmail.com>
2020-06-21 11:53:26 +0530

    map

--- lib/thirdscreen.dart
+++ lib/thirdscreen.dart
@@ -1,17 +1,17 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'secondscreen.dart';
 
-
-class ThirdScreen extends StatefulWidget{
+class ThirdScreen extends StatefulWidget {
   // ThirdScreen() : super();
   // final String title =" Maps";
   @override
   MapsState createState() => MapsState();
-
 }
+
 class MapsState extends State<ThirdScreen> {
-  Completer<GoogleMapController> _controller =Completer();
+  Completer<GoogleMapController> _controller = Completer();
   // static const LatLng _center =const LatLng(45.521563, -122.677433);
   // final Set<Marker> _markers ={};
   // LatLng _lastMapPosition = _center;
@@ -26,35 +26,79 @@ class MapsState extends State<ThirdScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Group ID"),
-        ),
-        
-        // body: Stack(
-        //   children: <Widget>[
-        //     GoogleMap(
-        //       onMapCreated: _onMapCreated,
-              // initialCameraPosition: CameraPosition(target: _center,zoom: 11.0,),
-        //       mapType: _currentMapType,
-        //       markers: _markers,
-        //       onCameraMove: _onCameraMove,
-        //     ),
-        //   ],
-        // )
-        body: 
-        Stack(
-          children: <Widget>[
-            Container(
+      appBar: AppBar(
+        title: Text("Group ID"),
+      ),
+
+      // body: Stack(
+      //   children: <Widget>[
+      //     GoogleMap(
+      //       onMapCreated: _onMapCreated,
+      // initialCameraPosition: CameraPosition(target: _center,zoom: 11.0,),
+      //       mapType: _currentMapType,
+      //       markers: _markers,
+      //       onCameraMove: _onCameraMove,
+      //     ),
+      //   ],
+      // )
+      body: Stack(
+        children: <Widget>[
+          Container(
               height: MediaQuery.of(context).size.height,
               width: MediaQuery.of(context).size.width,
-              child:GoogleMap(initialCameraPosition: CameraPosition(target: LatLng(28.644800,77.216721),zoom:12),
-              onMapCreated: (GoogleMapController controller){
-                _controller.complete(controller);
-              },
-              )
-            )
-          ],),
-        );
+              child: GoogleMap(
+                initialCameraPosition: CameraPosition(
+                    target: LatLng(28.644800, 77.216721), zoom: 12),
+                onMapCreated: (GoogleMapController controller) {
+                  _controller.complete(controller);
+                },
+              ))
+        ],
+      ),
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.white,
+        child: Container(
+          height: 40,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              
+    IconButton(
+      icon: Icon(
+        Icons.group,
+        color: Colors.black,
+      ),
+      onPressed: () {
+        // do something
+      },
+    ),
+              
+              OutlineButton.icon(
+                      label: Text(
+                        "end",
+                        style: TextStyle(fontSize: 20, color: Colors.red),
+                      ),
+                      icon: Icon(
+                        Icons.cancel,
+                        color: Colors.red,
+                      ),
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => SecondScreen()));
+                      },
+                      shape: new RoundedRectangleBorder(
+                        borderRadius: new BorderRadius.circular(8.0),
+                      ),
+                    ),
+            ],
+
+
+          ),
+        ),
+      ),
+    );
   }
 }
 // AIzaSyA_8Jw4i9r9i4ADetiZPqwYVQpDlguj61E
